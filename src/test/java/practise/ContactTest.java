93bef36d261a7adfb9c10060d3490dcc55b3ab68
ravikumarbachu79 <ravikumarbachu79@gmail.com>
2022-11-16 15:04:10 +0530

    new addition

---
+++ src/test/java/practise/ContactTest.java
@@ -0,0 +1,10 @@
+package practise;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+public void Contact() {
+	System.out.println("contact created");
+}
+}
---
+++ src/test/java/practise/CreateLeadTest.java
@@ -0,0 +1,14 @@
+package practise;
+import org.testng.annotations.Test;
+
+
+public class CreateLeadTest {
+
+	@Test
+	public void Create() {
+		System.out.println("lead created");
+	}
+		
+	}
+
+
